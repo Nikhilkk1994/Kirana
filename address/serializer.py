8a1e4bdcdc28fae235d26d9613509f0c0b4fabfb
Nikhil <nkaplas@amazon.com>
2020-06-20 12:13:06 +0530

    Add Address Add Api

--- address/serializer.py
+++ address/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers as rest_serializers
 
 from address import models  as address_models
+from customer import models as customer_models
 
 
 class UserAddressDetailSerializer(rest_serializers.ModelSerializer):
@@ -21,3 +22,35 @@ class UserAddressSerializer(rest_serializers.ModelSerializer):
     class Meta:
         model = address_models.Address
         fields = ('area_house_number', 'landmark', 'address_detail',)
+
+
+class UserAddressCreateSerializer(rest_serializers.Serializer):
+    """
+    Serializer to create the new Address
+    """
+    area_house_number = rest_serializers.CharField(help_text='address details', max_length=50)
+    landmark = rest_serializers.CharField(help_text='landmark', max_length=50, required=False)
+    state = rest_serializers.CharField(help_text='state', max_length=50)
+    country = rest_serializers.CharField(help_text='country', max_length=50)
+    zip_code = rest_serializers.CharField(help_text='zip code', max_length=50)
+
+    def create(self, validated_data):
+        """
+        To create the new Adrress for the user
+        :param validated_data: Validate data
+        :return: instance of the address
+        """
+        address_detail = address_models.AddressDetail.objects.get_or_create(
+            state=validated_data.get('state').lower(), country=validated_data.get('country').lower(),
+            zip_code=validated_data.get('zip_code').lower()
+        )
+        print(address_detail)
+        address = address_models.Address.objects.get_or_create(
+            area_house_number=validated_data.get('area_house_number').lower(),
+            landmark=validated_data.get('landmark').lower(), address_detail=address_detail[0]
+        )
+        # create address to user
+        address_to_user = customer_models.UserToAddress.objects.get_or_create(
+            user=self.context.get('request').user, address=address[0]
+        )
+        return address_to_user
--- address/views.py
+++ address/views.py
@@ -2,18 +2,33 @@ from __future__ import unicode_literals
 
 from rest_framework import mixins as rest_mixins
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.response import Response
+from rest_framework import status as http_status
 from rest_framework import permissions as rest_permissions
 
 from address import models as address_models
 from address import serializer as address_serializer
+from address import mixins as address_mixins
 
 
-class UserAddressView(rest_mixins.ListModelMixin, GenericViewSet):
+class UserAddressView(
+    rest_mixins.ListModelMixin, rest_mixins.CreateModelMixin, address_mixins.ActionSpecificSerializerMixin, GenericViewSet
+):
     """
     View Set for get the list of address of the Users
     """
     permission_classes = (rest_permissions.IsAuthenticated,)
-    serializer_class = address_serializer.UserAddressSerializer
+    serializer_classes = {
+        'list': address_serializer.UserAddressSerializer,
+        'create': address_serializer.UserAddressCreateSerializer
+    }
 
     def get_queryset(self):
         return address_models.Address.objects.filter(user__id=self.request.user.id)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        serializer = address_serializer.UserAddressSerializer(self.get_queryset(), many=True)
+        return Response(serializer.data, status=http_status.HTTP_201_CREATED)
