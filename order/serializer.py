0d43a8a67a17fc374e4b23ed805f7635bbcf7c8c
Nikhil <nkaplas@amazon.com>
2020-08-25 07:43:08 +0530

    Reducing the Database hit for ATC

--- order/serializer.py
+++ order/serializer.py
@@ -10,63 +10,39 @@ class CartCheckoutListSerializer(rest_serializers.ListSerializer):
     """
     def validate(self, attrs):
         # validate the bulk objects at once
-        return attrs
+        attrs = list(set(attrs))
+        # create the dict of the product
+        product_dict = {}
+        for data in attrs:
+            key = ''.join([str(data[0]), str(data[1])])
+            product_dict[key] = data[2]
+        product = list(merchant_models.MerchantProductPersonality.objects.filter(
+            merchant_product__in=[value[0] for value in attrs],
+            product_personality__in=[value[1] for value in attrs],
+            merchant_product__merchant=self.context.get('merchant_id'),
+        ).values('inventory', 'merchant_product_id', 'product_personality_id'))
+        # validate the product Inventory
+        for data in product:
+            key = ''.join([str(data['merchant_product_id']), str(data['product_personality_id'])])
+            if product_dict[key] <= data['inventory']:
+                data['inventory'] = product_dict[key]
+        return product
 
 
 class CartCheckoutSerializer(rest_serializers.Serializer):
     """
     Serializer for Cart Checkout
     """
-    merchant_product_id = rest_serializers.IntegerField(required=False, label=_('Merchant Product ID'))
-    inventory = rest_serializers.IntegerField(required=False, label=_('Inventory of the Product'))
-    product_personality_id = rest_serializers.IntegerField(required=False, label=_('Product Personality ID'))
-    status = rest_serializers.CharField(required=False, label=_('Error Status'), default='')
-    message = rest_serializers.CharField(required=False, label=_('ATC message'), default='')
+    merchant_product_id = rest_serializers.IntegerField(required=True, label=_('Merchant Product ID'))
+    inventory = rest_serializers.IntegerField(required=True, label=_('Inventory of the Product'))
+    product_personality_id = rest_serializers.IntegerField(required=True, label=_('Product Personality ID'))
 
     class Meta:
         list_serializer_class = CartCheckoutListSerializer
 
     def validate(self, attrs):
         # validate the single object
-        attrs = self.validate_all_required_fields(attrs)
-        if attrs.get('status') == 200:
-            # verify the product is available and belong to same merchant
-            product = merchant_models.MerchantProductPersonality.objects.filter(
-                merchant_product=attrs.get('merchant_product_id'),
-                product_personality=attrs.get('product_personality_id'),
-                merchant_product__merchant=self.context.get('merchant_id')
-            )
-            if product.exists():
-                if attrs.get('inventory') > product.first().inventory:
-                    attrs['message'] = 'Product is in shortage, decrease the inventory to {}'.format(
-                        product.first().inventory
-                    )
-                    attrs['inventory'] = product.first().inventory
-                    if attrs['inventory'] == 0:
-                        attrs['status'] = 404
-                        attrs['message'] = 'Out Of Stock'
-            else:
-                attrs['status'] = 404
-                attrs['message'] = 'Product not exists, Not belong to merchant with ID {}'.format(
-                    self.context.get('merchant_id')
-                )
-        return attrs
-
-    def validate_all_required_fields(self, data):
-        """
-        Verify that object have all required field
-        """
-        result = []
-        if not data.get('merchant_product_id', None):
-            result.append('merchant_product_id')
-        if not data.get('inventory', None):
-            result.append('inventory')
-        if not data.get('product_personality_id', None):
-            result.append('product_personality_id')
-        if len(result):
-            data['status'], data['message'] = (
-                400, 'error, here are list of fields that are required {} '.format(', '.join(result))
-            )
-            return data
-        data['status'], data['message'] = 200, 'Add To Cart check success'
-        return data
+        return (
+            attrs.get('merchant_product_id', None), attrs.get('product_personality_id', None),
+            attrs.get('inventory', 0)
+        )
