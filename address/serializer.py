ed0bcb64a5cbc676c9b72683acde768003b49f29
Nikhil <nkaplas@amazon.com>
2020-09-19 16:36:59 +0530

    add update and delete api

--- address/serializer.py
+++ address/serializer.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers as rest_serializers
+from rest_framework import exceptions as rest_exceptions
 
 from address import models  as address_models
 from customer import models as customer_models
@@ -45,13 +46,33 @@ class UserAddressCreateSerializer(rest_serializers.Serializer):
             state=validated_data.get('state').lower(), country=validated_data.get('country').lower(),
             zip_code=validated_data.get('zip_code').lower(), city=validated_data.get('city').lower()
         )
-        print(address_detail)
         address = address_models.Address.objects.get_or_create(
             area_house_number=validated_data.get('area_house_number').lower(),
             landmark=validated_data.get('landmark').lower(), address_detail=address_detail[0]
         )
         # create address to user
-        address_to_user = customer_models.UserToAddress.objects.get_or_create(
-            user=self.context.get('request').user, address=address[0]
+        customer_models.UserToAddress.objects.get_or_create(user=self.context.get('request').user, address=address[0])
+        return address
+
+    def update(self, instance, validated_data):
+        """
+        Update the address of the user
+        :param instance: instance of the Address
+        :param validated_data: validate data
+        :return: instance of the Address
+        """
+        address_detail = address_models.AddressDetail.objects.get_or_create(
+            state=validated_data.get('state').lower(), country=validated_data.get('country').lower(),
+            zip_code=validated_data.get('zip_code').lower(), city=validated_data.get('city').lower()
         )
-        return address_to_user
+        address = address_models.Address.objects.get_or_create(
+            area_house_number=validated_data.get('area_house_number').lower(),
+            landmark=validated_data.get('landmark').lower(), address_detail=address_detail[0]
+        )[0]
+        # delete the prev entry
+        customer_models.UserToAddress.objects.filter(
+            user=self.context.get('request').user, address=instance
+        ).first().delete()
+        # add the address of the user
+        customer_models.UserToAddress.objects.get_or_create(user=self.context.get('request').user, address=address)
+        return address
--- address/views.py
+++ address/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 from rest_framework import mixins as rest_mixins
 from rest_framework.viewsets import GenericViewSet
@@ -10,6 +11,7 @@ from rest_framework import permissions as rest_permissions
 from address import models as address_models
 from address import serializer as address_serializer
 from address import mixins as address_mixins
+from customer import models as customer_models
 
 
 class UserAddressView(
@@ -22,13 +24,17 @@ class UserAddressView(
     permission_classes = (rest_permissions.IsAuthenticated,)
     serializer_classes = {
         'list': address_serializer.UserAddressSerializer,
-        'create': address_serializer.UserAddressCreateSerializer
+        'create': address_serializer.UserAddressCreateSerializer,
+        'update': address_serializer.UserAddressCreateSerializer
     }
-    lookup_field = 'address_id'
     pagination_class = None
 
     def get_queryset(self):
-        return address_models.Address.objects.filter(user__id=self.request.user.id)
+        query_filter = Q(user__id=self.request.user.id)
+        param_id = self.kwargs.get('pk', None)
+        if param_id:
+            query_filter &= Q(id=param_id)
+        return address_models.Address.objects.filter(query_filter)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -37,8 +43,16 @@ class UserAddressView(
         serializer = address_serializer.UserAddressSerializer(self.get_queryset(), many=True)
         return Response(serializer.data, status=http_status.HTTP_201_CREATED)
 
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        serializer = address_serializer.UserAddressSerializer(self.get_queryset(), many=True)
+        return Response(serializer.data, status=http_status.HTTP_201_CREATED)
+
     def destroy(self, request, *args, **kwargs):
         # check user have this address field
         address = get_object_or_404(self.get_queryset(), id=self.kwargs.get(self.lookup_field))
-        address.delete()
+        customer_models.UserToAddress.objects.filter(address=address, user=self.request.user.id).first().delete()
         return Response(status=http_status.HTTP_204_NO_CONTENT)
